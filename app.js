1d7711b6a4dcade09c37c205cc96813bfe0cb490
Temur200511 <tziyodullayev61@gmail.com>
2024-12-05 21:33:55 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,94 @@
+
+// const A = prompt("Enter A"); 
+// const B = prompt("Enter B"); 
+// const C = prompt("Enter C"); 
+
+// const distanceAB = Math.abs(A - B);
+// const distanceAC = Math.abs(A - C);
+
+// let EngyaqinNuqta;
+// let EngyaqinMasofa;
+
+// if (distanceAB < distanceAC) {
+//   EngyaqinNuqta = 'B';
+//   EngyaqinMasofa = distanceAB;
+// } else {
+//   EngyaqinNuqta = 'C';
+//   EngyaqinMasofa = distanceAC;
+// }
+
+// console.log(`A nuqtaga eng yaqin nuqta: ${EngyaqinNuqta}`);
+// console.log(`A va ${EngyaqinNuqta} orasidagi masofa: ${EngyaqinMasofa}`);
+
+
+
+// const A = 10;
+// const B = 5;
+// const amal = 3;
+
+// let result;
+
+// switch (amal) {
+//   case 1:
+//     result = A + B;
+//     break;
+//   case 2:
+//     result = A - B;
+//     break;
+//   case 3:
+//     result = A / B;
+//     break;
+//   case 4:
+//     result = A * B;
+//     break;
+//   default:
+//     result = 'Noto‘g‘ri amal';
+// }
+
+// console.log(`Natija: ${result}`);
+
+
+
+
+
+
+
+// const element = 1; 
+// const qiymat = 7; 
+// const pi = 3.14;
+
+// let radius, diameter, uzunlik, yuzasi;
+
+// switch (element) {
+//   case 1:
+//     radius = qiymat;
+//     diameter = 2 * radius;
+//     uzunlik = 2 * pi * radius;
+//     yuzasi = pi * radius ** 2;
+//     break;
+//   case 2:
+//     diameter = qiymat;
+//     radius = diameter / 2;
+//     uzunlik = 2 * pi * radius;
+//     yuzasi = pi * radius ** 2;
+//     break;
+//   case 3:
+//     uzunlik = qiymat;
+//     radius = uzunlik / (2 * pi);
+//     diameter = 2 * radius;
+//     yuzasi = pi * radius ** 2;
+//     break;
+//   case 4:
+//     yuzasi = qiymat;
+//     radius = Math.sqrt(yuzasi / pi);
+//     diameter = 2 * radius;
+//     uzunlik = 2 * pi * radius;
+//     break;
+//   default:
+//     console.log('Noto‘g‘ri element turi');
+// }
+
+// console.log(`Radius: ${radius}`);
+// console.log(`Diametr: ${diameter}`);
+// console.log(`Uzunlik: ${uzunlik}`);
+// console.log(`Yuzasi: ${yuzasi}`);
